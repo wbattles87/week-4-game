07d5ff606e7909199648bf8e7ae7342236939751
Will <wbattles87@yahoo.com>
2017-10-14 22:43:17 -0500

    ok

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,56 @@
 $(document).ready(function() 
 {
+	var userscore =0;
+	var random = Math.floor(Math.random() * 120) + 19;
+	var random1 = Math.floor(Math.random() * (12)) + 1;
+	var random2 = Math.floor(Math.random() * (12)) + 1;
+	var random3 = Math.floor(Math.random() * (12)) + 1;
+	var random4 = Math.floor(Math.random() * (12)) + 1;
+
+
+
 	$("#buttonGenerator").on("click", function() 
 	{
-    	var random = Math.floor(Math.random() * 100) + 1;
-        alert("Your number is " + random);
-     });
- 
+		$("#numDisplay").text(random);
+	});
+
+	$("#gem1").on("click", function() 
+	{
+		$(".currentnumbercounter").text(random1);
+		$(".currentscore").text(random1+random2+random3+random4);
+	});
+
+	$("#gem2").on("click", function() 
+	{
+		$(".currentnumbercounter").text(random2);
+		$(".currentscore").text(random1+random2+random3+random4);
+	});
+
+
+	$("#gem3").on("click", function() 
+	{
+		$(".currentnumbercounter").text(random3);
+		$(".currentscore").text(random1+random2+random3+random4);
+	});
+
+
+	$("#gem4").on("click", function() 
+	{
+		$(".currentnumbercounter").text(random4);
+		$(".currentscore").text(random1+random2+random3+random4);
+	});
+
+	$("#gem1").click(function()
+	{
+		userscore +=  
+	});
+
+
+
+
+
+
 });
-
-
 
 
 
