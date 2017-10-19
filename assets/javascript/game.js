c795bfe05d97953a619e82118f0e26578ed8d8c2
Will <wbattles87@yahoo.com>
2017-10-19 01:53:59 -0500

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@
 $(document).ready(function() 
 {
 	var userscore = 0;
+	var wins = 0;
+	var losses = 0;
 	var random = Math.floor(Math.random() * 120) + 19;
 	var random1 = Math.floor(Math.random() * (12)) + 1;
 	var random2 = Math.floor(Math.random() * (12)) + 1;
@@ -14,7 +16,10 @@ $(document).ready(function()
 	//calling html button gem 1
 	//uses attr to storing random1 into value
 
-	$("#gem1").attr("value", random1)
+	$("#gem1").attr("value", random1);
+	$("#gem2").attr("value", random2);
+	$("#gem3").attr("value", random3);
+	$("#gem4").attr("value", random4);
 
 
 
@@ -28,29 +33,36 @@ $(document).ready(function()
 	// when you use this, it will call the #gem1 object inside the $function because its the next highest one
 	$("#gem1").on("click", function() 
 	{
-		$(".currentnumbercounter").text(random1);
 		var userclick1 = $(this).attr("value");
+		$(".currentnumbercounter").text(random1);
+		
 		// call a compare function here for each gem 1 2 3 4
-		//compareFunction(userclick1);
+		compareFunction(userclick1);
 	});
 
 	$("#gem2").on("click", function() 
 	{
+		var userclick2 = $(this).attr("value");
 		$(".currentnumbercounter").text(random2);
+		compareFunction(userclick2);
 
 	});
 
 
 	$("#gem3").on("click", function() 
 	{
+		var userclick3 = $(this).attr("value");
 		$(".currentnumbercounter").text(random3);
+		compareFunction(userclick3);
 
 	});
 
 
 	$("#gem4").on("click", function() 
 	{
+		var userclick4 = $(this).attr("value");
 		$(".currentnumbercounter").text(random4);
+		compareFunction(userclick4);
 
 	});
 
@@ -60,15 +72,37 @@ $(document).ready(function()
 	//	inside of each one, we are calling the var we associated in each one (userclick1 userclick2)
 	//  then userclickvalue, each time that is called, userclick1 userclick2 will take its place 
 	
-	//function compareFunction(userClickValue)
-	//{
-	//	userscore += userClickValue;
+	function compareFunction(userClickValue)
+	{
+		userscore += userClickValue;
+		$(".currentscore").text(userscore);
 
+		if (userscore === random)
+		{
+			$(".currentscore").text("You Win!");
+			wins++;
+
+		}
+		else if (userscore < random) 
+		{
+			$(".currentscore").text(userscore);
+		}
+		else
+		{
+			$(".currentscore").text("Sorry you lose");
+			losses++;
+
+		}
 
 		// dont forget compare function for equal to, greater then or less then for the user score
 		// also do the win loss function
 		// do a reset function but outside this portion
-	//};
+	};
+
+	function resetGame()
+	{
+
+	}
 
 });
 
